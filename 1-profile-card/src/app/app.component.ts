09aa97eb01f2ff56f527bd5d6e685c7e8d9bcc50
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-05-21 13:07:54 +0600

    project 1-profile-card completed

--- 1-profile-card/src/app/app.component.ts
+++ 1-profile-card/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = '1-profile-card';
+  name: string = '';
+  age: number | undefined;
+  description: string = '';
 }
