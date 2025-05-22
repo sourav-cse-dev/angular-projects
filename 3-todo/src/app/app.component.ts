2772590e05796b900cc54cacc86ab1f8cdc06d4c
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-05-22 14:38:59 +0600

    project 3-todo completed

--- 3-todo/src/app/app.component.ts
+++ 3-todo/src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TodoListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = '3-todo';
 }
---
+++ 3-todo/src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,26 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo-list',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todo-list.component.html',
+  styleUrl: './todo-list.component.scss',
+})
+export class TodoListComponent {
+  tasks: string[] = [];
+  newTask: string = '';
+
+  addTask() {
+    if (!this.newTask.trim()) {
+      return;
+    }
+    this.tasks.push(this.newTask);
+    this.newTask = '';
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+}
