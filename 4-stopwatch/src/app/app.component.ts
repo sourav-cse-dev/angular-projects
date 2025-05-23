1aaa6315291cd3c1e93bf0d6895c149176168fab
Sourav Chowdhury <sourav.seu.cse@gmail.com>
2025-05-23 11:54:29 +0600

    project 4-stopwatch completed

--- 4-stopwatch/src/app/app.component.ts
+++ 4-stopwatch/src/app/app.component.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { StopwatchComponent } from './stopwatch/stopwatch.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [StopwatchComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = '4-stopwatch';
-}
+export class AppComponent {}
